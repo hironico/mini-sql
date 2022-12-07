d5578713e62e03ecdac16052787548f6ae57d861
Nicolas RAMOUSSE <Nicolas.RAMOUSSE@ing.com>
2022-12-07 16:28:14 +0100

    use of jackson for XNL files as well in order to get ready for java 9+

--- hironico-common/src/main/java/net/hironico/common/utils/XMLFile.java
+++ hironico-common/src/main/java/net/hironico/common/utils/XMLFile.java
@@ -9,12 +9,20 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class XMLFile {
+    
+    private static final XmlMapper XML_MAPPER = getXmlMapper();
+    
+    private static XmlMapper getXmlMapper() {
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        xmlMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        xmlMapper.enable(JsonParser.Feature.IGNORE_UNDEFINED);
+        return xmlMapper;
+    } 
 
     public static String serialize(Object obj) throws IOException {
-        try {
-            XmlMapper xmlMapper = new XmlMapper();
-            return xmlMapper.writeValueAsString(obj);
-
+        try {            
+            return XML_MAPPER.writeValueAsString(obj);
         } catch (Exception ex) {
             throw new IOException(ex); 
         }
@@ -22,20 +30,15 @@ public class XMLFile {
 
     public static void saveAs(File file, Object obj) throws IOException {
         try {
-            XmlMapper xmlMapper = new XmlMapper();
-            xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
-            xmlMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-            xmlMapper.enable(JsonParser.Feature.IGNORE_UNDEFINED);
-            xmlMapper.writeValue(file, obj);
+            XML_MAPPER.writeValue(file, obj);
         } catch (Exception ex) {
             throw new IOException(ex);
         }
     }
 
     public static <T> T load(InputStream in, Class<T> clazz) throws IOException {
-        try (InputStream source = in) {
-            XmlMapper xmlMapper = new XmlMapper();
-            return xmlMapper.readValue(source, clazz);
+        try (InputStream source = in) {            
+            return XML_MAPPER.readValue(source, clazz);
         } catch (Exception ex) {
             throw new IOException(ex);
         }
@@ -43,8 +46,7 @@ public class XMLFile {
 
     public static <T> T load(File file, Class<T> clazz) throws IOException {
         try {
-            XmlMapper xmlMapper = new XmlMapper();
-            return xmlMapper.readValue(file, clazz);
+            return XML_MAPPER.readValue(file, clazz);
         } catch (Exception ex) {
             throw new IOException(ex);
         }
