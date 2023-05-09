9ae361d39a450f0584ad3d5fef76611300e1076c
hironico <hironico@outlook.com>
2023-05-09 15:23:38 +0200

    Added convertion of Enumeration to a stream

--- hironico-common/src/main/java/net/hironico/common/utils/StreamUtils.java
+++ hironico-common/src/main/java/net/hironico/common/utils/StreamUtils.java
@@ -1,8 +1,14 @@
 package net.hironico.common.utils;
 
+import java.util.Enumeration;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.concurrent.Callable;
+import java.util.function.Consumer;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * JAVA 8+ happy developpers can use these super usefull streams utils to get their life easier.
@@ -49,5 +55,41 @@ public class StreamUtils {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static class EnumerationSpliterator<T> extends Spliterators.AbstractSpliterator<T> {
+
+        private final Enumeration<T> enumeration;
+
+        public EnumerationSpliterator(long est, int additionalCharacteristics, Enumeration<T> enumeration) {
+            super(est, additionalCharacteristics);
+            this.enumeration = enumeration;
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super T> action) {
+            if (enumeration.hasMoreElements()) {
+                action.accept(enumeration.nextElement());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super T> action) {
+            while (enumeration.hasMoreElements())
+                action.accept(enumeration.nextElement());
+        }
+    }
+
+    /**
+     * Convert an Enumeration into Stream using Spliterator
+     * @param enumeration to convert into Stream
+     * @return a Stream containing the enumeration's elements of type T
+     * @param <T> the type of the enumeration elements
+     */
+    public static <T> Stream<T> stream(Enumeration<T> enumeration) {
+        EnumerationSpliterator<T> spliterator = new EnumerationSpliterator<T>(Long.MAX_VALUE, Spliterator.ORDERED, enumeration);
+        return StreamSupport.stream(spliterator, false);
     }
 }
