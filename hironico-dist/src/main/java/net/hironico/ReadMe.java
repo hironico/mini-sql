088e39c0aabe99e87768bd76e56a445ede7be1a0
hironico <hironico@outlook.com>
2024-05-24 13:23:17 +0200

    distribution project ready to publish into central repo

---
+++ hironico-dist/src/main/java/net/hironico/ReadMe.java
@@ -0,0 +1,9 @@
+package net.hironico;
+
+/**
+ * This class is a placeholder for the java doc jar to comply to the
+ * publishing rules of maven central. For javadoc of all the product, see
+ * the following modules : hironico-common, hironico-minisql
+ */
+public class ReadMe {
+}
---
+++ hironico-dist/src/main/resources/README.txt
@@ -0,0 +1,7 @@
+This is a place holder for source folder to comply with maven central deployment rules.
+
+See project's modules :
+- hironico-common
+- hironico-minisql
+
+for sources included in this uber JAR.
