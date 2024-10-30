a33ef6bf269ec72f7c4d804ff2f41019a858b970
hironico <hironico.pub@outlook.com>
2024-10-30 11:59:45 +0100

    support for quoted identifiers flag in db config

--- hironico-minisql/src/main/java/net/hironico/minisql/ui/dbexplorer/action/DbObjectCountAction.java
+++ hironico-minisql/src/main/java/net/hironico/minisql/ui/dbexplorer/action/DbObjectCountAction.java
@@ -1,17 +1,20 @@
 package net.hironico.minisql.ui.dbexplorer.action;
 
+import net.hironico.minisql.DbConfig;
+import net.hironico.minisql.DbConfigFile;
 import net.hironico.minisql.model.SQLObject;
 import net.hironico.minisql.model.SQLObjectTypeEnum;
 import net.hironico.minisql.ui.ExecuteQueryAction;
-import net.hironico.minisql.ui.MainWindow;
 import net.hironico.minisql.ui.editor.QueryPanel;
 
 import java.awt.event.ActionEvent;
 
 public class DbObjectCountAction extends AbstractDbExplorerAction {
-    private static final long serialVersionUID = 1L;
 
     public static final String NAME = "Count";
+
+    private static final String countQuery = "SELECT count(*) FROM %s.%s";
+    private static final String countQueryQuoted = "SELECT count(*) from \"%s\".\"%s\"";
 
     public DbObjectCountAction() {
         super(NAME, "icons8_counter_64px.png");
@@ -19,18 +22,27 @@ public class DbObjectCountAction extends AbstractDbExplorerAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        QueryPanel pnl = showNewQueryPanel();
-
-        SQLObject obj = MainWindow.getInstance().getSchemaExcplorerPanel().getSelectedSQLObject();
+        SQLObject obj = this.getExplorerPanel().getSelectedSQLObject();
         if (obj == null) {
             return;
         }
 
+        final DbConfig configToUse = DbConfigFile.getConfig(this.getExplorerPanel().getSelectedConnectionName());
+        if (configToUse == null) {
+            return;
+        }
+
+        QueryPanel pnl = this.showNewQueryPanel();
+
         if (SQLObjectTypeEnum.isTableOrView(obj.type)) {
-            String query = "SELECT count(*) FROM " + obj.schemaName + "." + obj.name;
+            String query = String.format(getCountQuery(configToUse.useQuotedIdentifiers), obj.schemaName, obj.name);
             pnl.setQueryText(query);
             pnl.setDividerLocation(0.25d);
             ExecuteQueryAction.executeQueryAsync(pnl);
         }
+    }
+
+    private String getCountQuery(boolean useQuotedIdentifiers) {
+        return useQuotedIdentifiers ? countQueryQuoted : countQuery;
     }
 }
